4d31ce4daca7ebec462ce94e541e6195b86021f0
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-13 09:44:17 +0300

    commit tree 71ae653b20da0401fba8c57c41478016426fe856

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// const initialValue = 0;
+// const sumWithInitial = batteryBatches.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
+
+// function reducer(previous, current, index, batteryBatches) {
+//     const totalBatteries = previous + current;
+//     console.log(`previous: ${previous}, current: ${current}, index: ${index}, returns: ${batteryBatches}`);
+//     return totalBatteries;
+//   }
+  
+//   batteryBatches.reduce(reducer);
+//   console.log(totalBatteries);
+
+
+let totalBatteries = batteryBatches.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue
+  }, 0)
+
+  console.log(totalBatteries);
